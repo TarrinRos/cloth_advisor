d9cdf54c9266376e03771d5d53a1b7c684553a88
TarrinRos <tarindis@gmail.com>
2018-10-14 11:10:12 +0300

    Change main.rb

--- main.rb
+++ main.rb
@@ -13,13 +13,11 @@ full_path = Dir.glob("#{current_path}/data/*.txt")
 smart_wardrobe = SmartWardrobe.new(full_path)
 
 puts 'Программа "Одевайтесь по погоде" v 1.0.3'
-
 puts '========================================'
 
 sleep 0.5
 
 puts 'Какая температура сейчас на улице?'
-
 puts 'Например: -20 или 20 или введите "х" для выхода'
 
 out_temp = STDIN.gets.chomp.downcase
@@ -30,11 +28,8 @@ if out_temp != 'x'
   case(out_temp)
   when(-40..40)
     puts ''
-
     puts 'В такую погоду я Вам советую надеть:'
-
     puts '===================================='
-
     puts ''
 
     smart_wardrobe.advise(out_temp)
