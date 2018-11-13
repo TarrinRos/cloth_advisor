79f63424407c42e4c6f4cbe91b17a36e8b6db32e
TarrinRos <tarindis@gmail.com>
2018-11-13 20:17:21 +0200

    Add initialize test for Cloth

--- spec/cloth_spec.rb
+++ spec/cloth_spec.rb
@@ -2,8 +2,15 @@ require 'rspec'
 require 'cloth'
 
 describe Cloth do
+  let(:cloth) { Cloth.new("spec/fixtures/cloth_sample.txt") }
   describe '#initialize' do
+    it 'returns title, type and temperature' do
+      temper = [-20, -5]
 
+      expect(cloth.title).to eq 'Куртка Gore-tex'
+      expect(cloth.type).to eq 'Тело'
+      expect(cloth.temper).to eq temper
+    end
   end
 
   describe '#for_weather?' do
