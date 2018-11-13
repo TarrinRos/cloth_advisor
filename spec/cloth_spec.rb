7a49e85fcdbbd61a496cfadabf26bc044cee816d
TarrinRos <tarindis@gmail.com>
2018-11-13 20:25:54 +0200

    Add .for_weather? test for Cloth

--- spec/cloth_spec.rb
+++ spec/cloth_spec.rb
@@ -3,18 +3,27 @@ require 'cloth'
 
 describe Cloth do
   let(:cloth) { Cloth.new("spec/fixtures/cloth_sample.txt") }
+
   describe '#initialize' do
-    it 'returns title, type and temperature' do
-      temper = [-20, -5]
+    it 'assings title, type and temperature from array' do
+      temper_range = [-20, -5]
 
       expect(cloth.title).to eq 'Куртка Gore-tex'
       expect(cloth.type).to eq 'Тело'
-      expect(cloth.temper).to eq temper
+      expect(cloth.temper).to eq temper_range
     end
   end
 
   describe '#for_weather?' do
+    it 'returns true if cloth is for weather' do
+      temper = -10
+      expect(cloth.for_weather?(temper)).to be_truthy
+    end
 
+    it 'returns false if cloth is not for weather`' do
+      temper = 25
+      expect(cloth.for_weather?(temper)).to be_falsey
+    end
   end
 
   describe '#to_s' do
